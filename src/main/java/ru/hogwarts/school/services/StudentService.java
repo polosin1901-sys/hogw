f937e9ef7af2927d24ee585306a071f840453c29
n.polosin <polosin1901@gmail.com>
2026-09-05 12:59:52 +0300

    hw1

---
+++ src/main/java/ru/hogwarts/school/services/StudentService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.StudentRepository;
+
+import java.util.Collection;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    private long lastId = 0;
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public Student getStudent(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Collection<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Collection<Student> findByAge(int age) {
+        return studentRepository.findByAge(age);
+    }
+}
